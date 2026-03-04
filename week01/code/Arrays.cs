9a532c61aa902ee953e2ee2bf11dee32042c72ba
Archibong Victor <varchibong428@gmail.com>
2026-03-04 03:37:14 -0600

    Week 1 - Arrays Task

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,21 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Plan:
+        // 1. Create an array of doubles with size equal to 'length' to hold the right number of mulitples
+         var result = new double[length];
+        // 2. Loop from 1 up to and including the 'length'. Each iteration i represents the ith multiple of the number 
+        for (int i = 1; i <= length; i++) {
 
-        return []; // replace this return statement with your own
+            // 3 & 4. On each iteration, multiply 'number' by i and store the result in the array at index i-1 (since arrays are 0 indexed)
+            result[i-1] = number * i;
+        }
+        // 4. After the loop, return the array of multiples
+        return result;
+
+        // 5. The time complexity of this function is O(n) where n is the length of the resulting array, 
+        // since we have to compute each multiple once. The space complexity is also O(n) since we are storing n multiples in the array.
+            
     }
 
     /// <summary>
@@ -29,5 +38,19 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // 1. Identify the slice that will omve to the front.
+
+        // 2. Calculate the split index - where the last "amount" elements starts 
+        var splitIndex = data.Count - amount;
+
+        // 3. Extract the last "amount" elements to be moved to the front and store in a temp list
+        var tail = data.GetRange(splitIndex, amount);
+
+        // 4. Remove the last "amount" elements from the original list
+        data.RemoveRange(splitIndex, amount);
+        
+        // 5. Insert the extracted elements at the beginning of the original list.
+        data.InsertRange(0, tail);
     }
 }
--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,19 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        int[] result = new int[select.Length];
+        int i1 = 0, i2 = 0;
+        for (int i = 0; i < select.Length; i++)
+        {
+            if (select[i] == 1)
+            {
+                result[i] = list1[i1++];
+            }
+            else
+            {
+                result[i] = list2[i2++];
+            }
+        }
+        return result;
     }
 }
