2318c8411bb1449c8e0231f87d3a1e5935f1346b
Archibong Victor <varchibong428@gmail.com>
2026-03-23 04:19:10 -0500

    Week 3 Learning on Sets and Maps

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -25,6 +25,17 @@
     private static int CountDuplicates(int[] data)
     {
         // Add code here.
-        return 0;
+        HashSet<int> seen = new HashSet<int>();
+        HashSet<int> duplicates = new HashSet<int>();
+        
+        foreach (int num in data)
+        {
+            if (!seen.Add(num))
+            {
+                duplicates.Add(num);
+            }
+        }
+        
+        return duplicates.Count;
     }
 }
